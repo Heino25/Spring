e6b294de10f7fc4d5403f125d54ac8e90f2253b4
Heino <heinoduplessis@gmail.com>
2021-11-30 08:52:37 +0200

    30/11/21

---
+++ demo (1)/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   // <1>
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args); // <2>
+  }
+
+}
