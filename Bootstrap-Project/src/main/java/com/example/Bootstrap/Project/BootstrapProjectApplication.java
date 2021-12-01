879a7c838a1d2426c445660efe58f0ec00b79cd6
Heino <heinoduplessis@gmail.com>
2021-12-01 15:53:58 +0200

    1/12/21

---
+++ Bootstrap-Project/src/main/java/com/example/Bootstrap/Project/BootstrapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bootstrap.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapProjectApplication.class, args);
+	}
+
+}
---
+++ ch3new/src/test/java/com/example/ch3new/Ch3newApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch3new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch3newApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
